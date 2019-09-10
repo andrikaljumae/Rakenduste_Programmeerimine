be37621fd1814a937eb128fa4eb55e0e4a2c5dfd
Andri Kaljumäe <43129486+andrikaljumae@users.noreply.github.com>
2019-09-10 13:46:45 +0300

    praktikum 2 ulesanne 1

--- static/item.js
+++ static/item.js
@@ -6,7 +6,7 @@
       const cost = urlParams.get("cost");
       const src = urlParams.get("src");
       console.log(title, cost, src);
-      alert(`Title: ${title} cost: ${cost} path: ${src}`)
+      //alert(`Title: ${title} cost: ${cost} path: ${src}`)
 
       const container = document.createElement("div");
       container.className = "itemContainer";
@@ -17,7 +17,7 @@
 
       const titleElement = document.createElement("h2");
       titleElement.textContent = title;
-      title.Element.className = "item_title";
+      titleElement.className = "item_title";
 
       const description = "siin on mingi lambine tekst";
       const textElement = document.createElement("p");
@@ -34,6 +34,6 @@
       container.append(costElement);
 
       window.addEventListener("load", () =>{
-        const app =document.getElementById("item-body");
+        const app = document.getElementById("item-body");
         app.append(container);
     });
