55d717a73ad5c7aef27bcf66ee269ba2bee60db3
Andri Kaljumäe <43129486+andrikaljumae@users.noreply.github.com>
2019-09-14 15:03:52 +0300

    praktikum 2 ülesanne 5

---
+++ static/utils.js
@@ -0,0 +1,27 @@
+function createItemElement(item){
+    const anchor = document.createElement("a");
+    anchor.href = `./item.html?title=${item.title}&cost=${item.price}&src=${item.imgSrc}`;
+
+    const itemContainer = document.createElement("div");
+    itemContainer.className = "item";
+
+    const imgElement = document.createElement("img");
+    imgElement.src = item.imgSrc;
+
+    const titleElement = document.createElement("div");
+    titleElement.textContent = item.title;
+    titleElement.className = "item_title";
+    
+
+    const priceElement = document.createElement("div");
+    priceElement.innerText = item.price;
+    priceElement.className = "item_price";
+
+    anchor.append(itemContainer);
+    itemContainer.append(imgElement);
+    itemContainer.append(titleElement);
+    itemContainer.append(priceElement);
+
+    return anchor;
+
+}
