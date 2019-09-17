4d8810c9df26c7fdffae385ee5b2a569223ff830
Andri Kaljumäe <43129486+andrikaljumae@users.noreply.github.com>
2019-09-17 14:30:03 +0300

    praktikum 3 ülesanne 6

--- src/index.jsx
+++ src/index.jsx
@@ -5,10 +5,6 @@ import ReactDOM from "react-dom";
 const root = document.getElementById("app");
 
 ReactDOM.render(
-  React.createElement(
-    "button",
-    {},
-    "I am a button, Hello world"
-  ),
-  root,
+  <button>Hello World</button>,
+  root
 );
