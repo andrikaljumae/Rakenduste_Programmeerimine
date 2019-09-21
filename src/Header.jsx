140fcfe28c9b068799cb81d90664efa6540f4550
Andri Kaljumäe <43129486+andrikaljumae@users.noreply.github.com>
2019-09-21 20:07:23 +0300

    praktikum 3 ülesanne 8

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
       <div className = "header">
+        <Link to ={"/"}>
         <img className="header_logo" src="./images/pilt.png"/>
+        </Link>
         <div className="header_buttons">
           <button>Login/signup</button>
           <button>Cart</button>
