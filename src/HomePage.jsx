be66d93c6f56803b62e9bd3601ec437474870ab6
Andri Kaljumäe <43129486+andrikaljumae@users.noreply.github.com>
2019-09-23 15:03:33 +0300

    praktikum 4 ül 5 pooleli

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {laptops, phones} from "./mydatabase.js";
+//import {laptops, phones} from "./mydatabase.js";
 import Header from "./Header.jsx";
 import ItemList from "./ItemList.jsx";
 
@@ -8,25 +8,46 @@ class HomePage extends React.PureComponent{
     constructor(props){
       super(props);
       this.state = {
-        items: phones,
+        items: [],
   
       };
     }
+componentDidMount(){
+  this.fetchItems();
+}
+fetchItems = () => {
+  fetch("http://localhost:9000/api/items")
+  .then(res => {
+    console.log("res", res);
+    return res.json();
+  })
+  .then( items => {
+    console.log("items", items);
+    this.setState({
+      items
+    });
+  })
+  .catch(err => {
+    console.log("err", err);
+  });
+};
+
     handleChange(event){
-      switch (event.target.value) {
-        case "phones":{
-          this.setState({
-            items: phones,
-          });
-          break;
-        }
-        case "laptops":{
-          this.setState({
-            items: laptops,
-          });
-          break;
-        }
-      }
+      console.log(event.target.value);
+      // switch (event.target.value) {
+      //   case "phones":{
+      //     this.setState({
+      //       items: phones,
+      //     });
+      //     break;
+      //   }
+      //   case "laptops":{
+      //     this.setState({
+      //       items: laptops,
+      //     });
+      //     break;
+      //   }
+      // }
     }
     render(){
       return (
