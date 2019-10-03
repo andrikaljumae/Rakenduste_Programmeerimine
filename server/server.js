1ded264399afdb3a85ef881c9bc48b3d5e298b8d
Andri Kaljumäe <43129486+andrikaljumae@users.noreply.github.com>
2019-10-03 21:12:19 +0300

    praktikum5: MongoDB

--- server/server.js
+++ server/server.js
@@ -3,6 +3,39 @@ const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3000;
 const DB = require("./database.js");
+const mongoose = require("mongoose");
+require('dotenv').config();
+
+
+var kittySchema = new mongoose.Schema({
+  name: String
+});
+
+var Kitten = mongoose.model('Kitten', kittySchema);
+
+const kitten1 = new Kitten({
+  name: "red cat 2"
+});
+
+const DB_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0-qfjwa.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+
+
+mongoose.connect(DB_URL)
+  .then(() => {
+    console.log("Database access success!");
+    kitten1.save( err => {
+      if(err) {
+        console.error("we had an error"); 
+      }
+      else {
+        console.log("Success save");
+      }
+    });
+  })
+  .catch( err => {
+    console.log("error happened", err);
+  });
 
 app.get("/api/items", (req, res)=>{
     res.json(DB.getItems());
