6cffead952bd68fc2694086fa56b5480c2545bcb
Andri Kaljumäe <43129486+andrikaljumae@users.noreply.github.com>
2019-10-06 14:29:15 +0300

    praktikum5: MongoDB POST/GET

--- server/server.js
+++ server/server.js
@@ -5,48 +5,12 @@ const PORT = process.env.PORT || 3000;
 const DB = require("./database.js");
 const mongoose = require("mongoose");
 require('dotenv').config();
+const userRouter = require("./user.js");
 
-// const result = require('dotenv').config();
-// console.log("result", result);
-
-
-var kittySchema = new mongoose.Schema({
-  name: String
-});
-
-var Kitten = mongoose.model('Kitten', kittySchema);
-
-const kitten1 = new Kitten({
-  name: "red cat 2"
-});
 
 const DB_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0-qfjwa.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
-
-
-mongoose.connect(DB_URL)
-  .then(() => {
-    console.log("Database access success!");
-    kitten1.save( err => {
-      if(err) {
-        console.error("we had an error"); 
-      }
-      else {
-        console.log("Success save");
-      }
-    });
-  })
-  .catch( err => {
-    console.log("error happened", err);
-  });
-
-app.get("/api/items", (req, res)=>{
-    res.json(DB.getItems());
-  });
-
-  app.get("/api/items/:itemId", (req, res)=>{
-    res.send(DB.getItem(req.params.itemId));
-  });
+app.use(userRouter);
 
 
 app.get('/', (req, res) => {
@@ -59,7 +23,19 @@ app.get('/items/*', (req, res) => {
 
 app.use(express.static('dist'));
 
-app.listen(PORT, () => {
+function listen() {
+  app.listen(PORT, () => {
     console.log("Server started", PORT);
     console.log('http://localhost:${PORT}');
   });
+}
+
+mongoose.connect(DB_URL)
+  .then(() => {
+    console.log("Database access success!");
+    listen();
+  })
+  .catch( err => {
+    console.log("error happened", err);
+  });
+
