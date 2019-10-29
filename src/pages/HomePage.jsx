b81348985120c59f89dac3206370ba34e5f56f7c
Andri Kaljumäe <43129486+andrikaljumae@users.noreply.github.com>
2019-10-29 11:18:54 +0200

    Praktikum 8: Refactor filter(ül1)

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -37,21 +37,25 @@ fetchItems = () => {
   });
 };
 
-    handleDropdown = (event) => {
-      console.log(event.target.value);
-      if(this.isSelected(event.target.name)){
-        const clone = this.state.selectedCategories.slice();
-        const index = this.state.selectedCategories.indexOf(event.target.name);
-        clone.splice(index, 1);
-        this.setState({
-          selectedCategories: clone
-        });
+    handleFilterSelect = (event) => {
+      const categoryName = event.target.name;
+      if(this.isSelected(categoryName)){
+        return this.unselectCategory(categoryName);
       }
-      else {
-        this.setState( {
-          selectedCategories: this.state.selectedCategories.concat([event.target.name])
-        });
-      }
+      this.selectCategory(categoryName);
+    };
+
+    selectCategory = (categoryName) => {
+      this.setState( {
+        selectedCategories: this.state.selectedCategories.concat([categoryName])
+      });
+    };
+
+    unselectCategory = (categoryName) => {
+      const newArr = this.state.selectedCategories.filter(cn => cn !== categoryName);
+      this.setState({
+        selectedCategories: newArr
+      });
     };
 
     getVisibleItems = () => {
@@ -81,7 +85,7 @@ fetchItems = () => {
           <div className={"filters-wrapper"}>
             <ItemFilters
               allCategories={this.state.allCategories}
-              handleDropdown={this.handleDropdown}
+              handleDropdown={this.handleFilterSelect}
               isSelected={this.isSelected}
             />
           </div>
