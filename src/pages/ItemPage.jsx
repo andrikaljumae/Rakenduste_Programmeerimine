d5f22f9deca4df7c5ef2733f2dec4eddfc0867a5
Andri Kaljumäe <43129486+andrikaljumae@users.noreply.github.com>
2019-11-05 16:39:19 +0200

    Praktikum 9: Buy button task3

--- src/pages/ItemPage.jsx
+++ src/pages/ItemPage.jsx
@@ -2,8 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./itempage.css";
 import FancyButton from "../components/FancyButton.jsx";
+import {connect} from "react-redux";
+import { addItem } from "../store/store";
 
 class ItemPage extends React.PureComponent{
+
+  static propTypes = {
+    dispatch: PropTypes.func.isRequired,
+  };
 
   constructor(props){
     super(props);
@@ -27,6 +33,10 @@ class ItemPage extends React.PureComponent{
       console.log("item page", err);
     });
   };
+
+  handleBuy = () => {
+    this.props.dispatch(addItem(this.state._id));  
+  }
     render(){
       return (
         <>
@@ -52,7 +62,7 @@ class ItemPage extends React.PureComponent{
             </div>
             </div>
             <div className={"itemPage-footer"}>
-              <FancyButton onClick={()=>0}>Osta</FancyButton>
+              <FancyButton onClick={this.handleBuy}>Osta</FancyButton>
             </div>
           </div>
         </>
@@ -64,6 +74,6 @@ class ItemPage extends React.PureComponent{
     match: PropTypes.object.isRequired,
   };
 
-  export default ItemPage;
+  export default connect()(ItemPage);
 
   const loremIpsum = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
